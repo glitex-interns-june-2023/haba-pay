478f24f38aa02d5e0f7152653e7749d9908f2a30
Mary83-ops <nabwiremaryann@gmail.com>
2023-07-18 13:47:56 +0300

    updating homepage interface

--- src/Components/RightSidebar.js
+++ src/Components/RightSidebar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import approved from '../Assets/approved.png';
 import pending from '../Assets/pending.png';
+import review from '../Assets/review.png';
+import user from '../Assets/user.png';
 import '../Styles/RightSidebar.css';
 
 const RightSidebar = () => {
@@ -18,11 +20,11 @@ const RightSidebar = () => {
                             <img src={approved} alt="" />
                             <div className="admin-details">
                                 <div className="admin-detail">
-                                    <div className="name"><h4>Jane Mukenya M.</h4></div>
+                                    <div className="name">Jane Mukenya M.</div>
                                     <div className="admin-number">+254712345678</div>
                                 </div>
                                 <div className="status-details">
-                                    <div className="amount">Ksh 240</div>
+                                    <div className="amount">Ksh <span>240</span></div>
                                     <div className="time">12:45PM</div>
                                 </div>
                             </div>
@@ -30,13 +32,13 @@ const RightSidebar = () => {
 
                         <div className="transaction-detail">
                             <img src={approved} alt="" />
-                            <div className="dmin-details">
+                            <div className="admin-details">
                                 <div className="admin-detail">
-                                    <h4>Obi Peter</h4>
+                                    <div className="name">Obi Peter</div>
                                     <div className="admin-number">+254712345678</div>
                                 </div>
                                 <div className="status-details">
-                                    <div className="amount">Ksh 4000</div>
+                                    <div className="amount">Ksh <span>4000</span></div>
                                     <div className="time">12:00PM</div>
                                 </div>
                             </div>
@@ -46,12 +48,12 @@ const RightSidebar = () => {
                             <img src={pending} alt="" />
                             <div className="admin-details">
                                 <div className="admin-detail">
-                                    <h4>Bola Tinubu</h4>
+                                    <div className="name">Bola Tinubu</div>
                                     <div className="admin-number">+254712345678</div>
                                 </div>
                             
-                                <div className="statusdetails">
-                                    <div className="amount">Ksh 40</div>
+                                <div className="status-details">
+                                    <div className="amount">Ksh <span>40</span></div>
                                     <div className="time">11:05AM</div>
                                 </div>
                             </div>
@@ -61,11 +63,11 @@ const RightSidebar = () => {
                             <img src={approved} alt="" />
                             <div className="admin-details">
                                 <div className="admin-detail">
-                                    <h4>John Doe</h4>
+                                    <div className="name">John Doe</div>
                                     <div className="admin-number">+254712345678</div>
                                 </div>
                                 <div className="status-details">
-                                    <div className="amount">Ksh 60</div>
+                                    <div className="amount">Ksh <span>60</span></div>
                                     <div className="time">10:05AM</div>
                                 </div>
                             </div>
@@ -75,11 +77,11 @@ const RightSidebar = () => {
                             <img src={pending} alt="" />
                             <div className="admin-details">
                                 <div className="admin-detail">
-                                    <h4>Client Mwilu</h4>
+                                    <div className="name">Client Mwilu</div>
                                     <div className="admin-number">+254712345678</div>
                                 </div>
                                 <div className="status-details">
-                                    <div className="amount">Ksh 100</div>
+                                    <div className="amount">Ksh <span>100</span></div>
                                     <div className="time">8:16AM</div>
                                 </div>
                             </div>
@@ -89,11 +91,11 @@ const RightSidebar = () => {
                             <img src={approved} alt="" />
                             <div className="admin-details">
                                 <div className="admin-detail">
-                                    <h4>Josephine Naruto</h4>
+                                    <div className="name">Josephine Naruto</div>
                                     <div className="admin-number">+254712345678</div>
                                 </div>
                                 <div className="status-details">
-                                    <div className="amount">Ksh 180</div>
+                                    <div className="amount">Ksh <span>180</span></div>
                                     <div className="time">5:55AM</div>
                                  </div>
                             </div>
@@ -103,14 +105,82 @@ const RightSidebar = () => {
                             <img src={pending} alt="" />
                             <div className="admin-details">
                                 <div className="admin-detail">
-                                    <h4>Grace Mwai</h4>
+                                    <div className="name">Grace Mwai</div>
                                     <div className="admin-number">+254712345678</div>
                                 </div>
                                 <div className="status-details">
-                                    <div className="amount">Ksh 750</div>
+                                    <div className="amount">Ksh <span>750</span></div>
                                     <div className="time">1:07PM</div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="users-reg">
+                <div className="new-details">
+                    <div className="details-title">New Users</div>
+                    <div className="user-view">View All <img src={review} alt="" /></div>
+                </div>
+
+                <div className="user-reg">
+                    <div className="date-reg">
+                        <h1>5th March 2023</h1>
+                    </div>
+
+                    <div className="reg-details">
+                        <div className="reg-detail">
+                            <div className="detail-name">Grace Mwai</div>
+                            <div className="detail-email">gracemwai444@haba.co.ke</div>
+                        </div>
+
+                        <div className="view">
+                            <img src={user} alt="" />
+                        </div>
+                    </div>
+
+                    <div className="reg-details">
+                        <div className="reg-detail">
+                            <div className="detail-name">Bola Tinubu</div>
+                            <div className="detail-email">bolatinubu00@outlook.com</div>
+                        </div>
+
+                        <div className="view">
+                            <img src={user} alt="" />
+                        </div>
+                    </div>
+
+                    <div className="reg-details">
+                        <div className="reg-detail">
+                            <div className="detail-name">Nancy Wajacoki</div>
+                            <div className="detail-email">nancywajacoki22@yahoo.com</div>
+                        </div>
+
+                        <div className="view">
+                            <img src={user} alt="" />
+                        </div>
+                    </div>
+
+                    <div className="reg-details">
+                        <div className="reg-detail">
+                            <div className="detail-name">Peter Obi</div>
+                            <div className="detail-email">obidientpeter@state.ng</div>
+                        </div>
+
+                        <div className="view">
+                            <img src={user} alt="" />
+                        </div>
+                    </div>
+
+                    <div className="reg-details">
+                        <div className="reg-detail">
+                            <div className="detail-name">Client Mwilu</div>
+                            <div className="detail-email">clientmwilu@gmail.com</div>
+                        </div>
+
+                        <div className="view">
+                            <img src={user} alt="" />
                         </div>
                     </div>
                 </div>
