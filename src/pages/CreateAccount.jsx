a9dddd7cde81bee492cbe4c2cc539e35e81b8cd4
Mary83-ops <nabwiremaryann7@gmail.com>
2023-08-28 13:32:25 +0300

    fix: Updated the create account component styling

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../Styles/CreateAccount.css';
 
 const CreateAccount = () => {
   const history = useNavigate();
