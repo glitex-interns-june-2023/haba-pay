531c9a8b8f24dc86bb776b1c835d00552654d855
Mary83-ops <nabwiremaryann7@gmail.com>
2023-08-29 12:07:53 +0300

    chore: Rename images for improved consistency

---
+++ src/Modals/WithdrawalModal.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import withdrawal from '../assets/withdrawal.png';
+import '../Styles/WithdrawalModal.css';
+
+const WithdrawalModal = () => {
+    return (
+        <div className="withdrawalmodal-container">
+            <div className="withdrawalmodal-wrapper">
+                <div className="withdrawalmodal-header">
+                    <h1>Transaction Details</h1>
+                    <h2><span><img src={withdrawal} alt="Withdrawal" /></span>Withdrawal</h2>
+                </div>
+
+                <div className="withdrawalmodal-line"></div>
+
+                <div className="withdrawalmodal-content">
+                    <div className="withdrawalmodal-request">
+                        <h2>Request by</h2>
+                        <h3>Jane Mukenya M.</h3>
+                    </div>
+
+                    <div className="withdrawalmodal-amount">
+                        <h2>Amount</h2>
+                        <h3>Ksh 240</h3>
+                    </div>
+
+                    <div className="withdrawalmodal-number">
+                        <h2>Number</h2>
+                        <h3>+254 712345678</h3>
+                    </div>
+
+                    <div className="withdrawalmodal-time">
+                        <h2>Time</h2>
+                        <h3>12:45 PM 5th March 2023</h3>
+                    </div>
+                </div>
+
+                <button className="withdrawalmodal-btn">Approve</button>
+            </div>
+        </div>
+    );
+};
+
+export default WithdrawalModal;
