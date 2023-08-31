e2f97832fa22d73367752943becc34005e4339a3
Mary83-ops <nabwiremaryann7@gmail.com>
2023-08-31 12:32:32 +0300

    feat: Implemented the dissolve animation to close button

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../Styles/VerifyEmail.css';
+import close from '../assets/close.png';
 
 function EmailVerification() {
+  const navigate = useNavigate();
   const [pin, setPin] = useState('');
+
+  const handleButtonClick = () => {
+    navigate('/abort');
+  };
 
   const handlePinChange = (event) => {
     setPin(event.target.value);
@@ -13,8 +21,16 @@ function EmailVerification() {
 
   return (
     <div className="verify-email">
-      <h1>Verify Email</h1>
-      <p>A verification PIN was sent to the email account you entered</p>
+      <div className="close-btn-container">
+        <div className="close-btn" onClick={handleButtonClick}>
+          <img src={close} alt="close" />
+        </div>
+      </div>
+
+      <div className="verify-email-header">
+        <h1>Verify Email</h1>
+        <p>A verification PIN was sent to the email account you entered</p>
+      </div>
       <form>
       <label htmlFor="pin">Verification Pin</label>
       <input
