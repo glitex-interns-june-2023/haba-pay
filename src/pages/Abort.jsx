ac3ec9e3a1f7af6cd73d4b1d149bbf90e8317c02
Mary83-ops <nabwiremaryann7@gmail.com>
2023-08-31 12:31:06 +0300

    feat: Updated the Abort component and added dissolve animation to close button

--- src/pages/Abort.jsx
+++ src/pages/Abort.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import '../Styles/Abort.css';
+import close from '../assets/close.png';
 
 const Abort = () => {
+  const navigate = useNavigate();
   const [isAborted, setIsAborted] = useState(false);
+
+  const handleButtonClick = () => {
+    navigate('/apppin');
+  };
 
   const handleAbortClick = () => {
     setIsAborted(true);
@@ -16,23 +24,29 @@ const Abort = () => {
 
   return (
     <div className="abort-process">
-      <h1>Abort Process</h1>
-      <div>
-        <p>
-          Information you may have entered will be saved by default. But you can{' '}
-          <span>abort and discard</span>
-        </p>
-      </div>
+        <div className="close-btn-container">
+            <div className="close-btn" onClick={handleButtonClick}>
+                <img src={close} alt="close" />
+            </div>
+        </div>
 
-      <div className="abort-btn">
-        <button onClick={handleResumeClick} disabled={isAborted} className="btn-resume">
-          Resume
-        </button>
-        <button onClick={handleAbortClick} disabled={isAborted} className="btn-abort">
-          Abort
-        </button>
-      </div>
-      <ToastContainer />
+        <div className="abort-header">
+          <h1>Abort Process</h1>
+          <p>
+            Information you may have entered will be saved by default. But you can{' '}
+            <span>abort and discard</span>
+          </p>
+        </div>
+
+        <div className="abort-btn">
+            <button onClick={handleResumeClick} disabled={isAborted} className="btn-resume">
+                Resume
+            </button>
+            <button onClick={handleAbortClick} disabled={isAborted} className="btn-abort">
+                Abort
+            </button>
+        </div>
+        <ToastContainer />
     </div>
   );
 };
